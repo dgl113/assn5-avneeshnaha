c317b9500a1228fbc4fc1fb529756be3c6a78216
avneeshnaha <144274551+avneeshnaha@users.noreply.github.com>
2024-03-03 02:09:24 -0800

    Write code

---
+++ docs/app.js
@@ -0,0 +1,77 @@
+'use strict';
+
+class OrderItem {
+    constructor(quantity, size, description) {
+        this._quantity = quantity;
+        this._size = size;
+        this._description = description;
+    }
+
+    get quantity() {
+        return this._quantity;
+    }
+
+    set quantity(value) {
+        this._quantity = value;
+    }
+
+    get size() {
+        return this._size;
+    }
+
+    set size(value) {
+        this._size = value;
+    }
+
+    get description() {
+        return this._description;
+    }
+
+    set description(value) {
+        this._description = value;
+    }
+
+    cost() {
+        let unitPrice;
+        // Determine unit price based on description
+        if (this._description.toLowerCase().includes('coffee')) {
+            switch (this._size.toLowerCase()) {
+                case 'short':
+                    unitPrice = 2.99;
+                    break;
+                case 'tall':
+                    unitPrice = 3.19;
+                    break;
+                case 'grande':
+                    unitPrice = 3.49;
+                    break;
+                case 'venti':
+                    unitPrice = 3.99;
+                    break;
+                default:
+                    throw new Error('Invalid size');
+            }
+        } else if (this._description.toLowerCase().includes('tea')) {
+            switch (this._size.toLowerCase()) {
+                case 'short':
+                    unitPrice = 2.85;
+                    break;
+                case 'tall':
+                    unitPrice = 3.05;
+                    break;
+                case 'grande':
+                    unitPrice = 3.25;
+                    break;
+                case 'venti':
+                    unitPrice = 3.50;
+                    break;
+                default:
+                    throw new Error('Invalid size');
+            }
+        } else {
+            throw new Error('Invalid description');
+        }
+        // Calculate total cost
+        return this._quantity * unitPrice;
+    }
+}
