aac9c016183c0f780aef908eebc884e84a8c27be
avneeshnaha <144274551+avneeshnaha@users.noreply.github.com>
2024-03-03 11:25:12 -0800

    Make adjustments to code

--- docs/app.js
+++ docs/app.js
@@ -1,50 +1,64 @@
+'use strict';
+
 class OrderItem {
   #quantity;
   #size;
   #description;
+
   constructor(quantity, size, description) {
     this.#quantity = quantity;
     this.#size = size;
     this.#description = description;
   }
+
   get quantity() {
     return this.#quantity;
   }
- get size() {
+
+  get size() {
     return this.#size;
   }
-get description() {
+
+  get description() {
     return this.#description;
   }
+
+  
   set quantity(value) {
     this.#quantity = value;
   }
-  set size(value) {
+
+ set size(value) {
     this.#size = value;
   }
- set description(value) {
+
+  set description(value) {
     this.#description;
   }
+
+
   cost() {
     const baseCosts = {
-      ‘Short (236ml)’: {
+      'Short (236ml)': {
         coffee: 2.99,
         tea: 2.85,
       },
-      ‘Tall (354ml)’: {
+      'Tall (354ml)': {
         coffee: 3.19,
         tea: 3.05,
       },
-      ‘Grande (473ml)’: {
+      'Grande (473ml)': {
         coffee: 3.49,
         tea: 3.25,
       },
-      ‘Venti (591ml)’: {
+      'Venti (591ml)': {
         coffee: 3.99,
         tea: 3.5,
       },
     };
-    const type = this.#description.includes(‘Tea’) ? ‘tea’ : ‘coffee’;
+
+
+    const type = this.#description.includes('Tea') ? 'tea' : 'coffee';
     const costPerItem = baseCosts[this.#size][type];
     return this.#quantity * costPerItem;
   }
